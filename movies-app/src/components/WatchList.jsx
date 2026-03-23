aa6a4f064e087ee5c89e67d5f2c3b41101525657
Lokesh Reddy <peddamallulokeswarareddy2001@gmail.com>
2026-03-23 08:14:01 +0530

    Filtering

--- movies-app/src/components/WatchList.jsx
+++ movies-app/src/components/WatchList.jsx
@@ -3,9 +3,18 @@ import genreids from "../constants/genreids";
 function WatchList(){
     const [watchlist,setWatchList]=useState([]);
     const [search, setSearch]=useState("");
+    const [genrelist,setGenreList]=useState(["All Genres","Thriller","Horror"]);
+    const [currGenre,setCurrGenre]=useState("All Genres");
     const handleSearch=(event)=>{
         setSearch(event.target.value);
     }
+    useEffect(()=>{
+        let temp=watchlist.map((movie)=>{
+            return genreids[movie.genre_ids[0]];
+        });
+        temp=new Set(temp);
+        setGenreList(["All Genres",...temp]);
+    },[watchlist]);
     useEffect(()=>{
         const moviesFromStorage=localStorage.getItem('movies');
         if(moviesFromStorage){
@@ -39,8 +48,19 @@ function WatchList(){
         });
         setWatchList([...sortedDescencending]);
     }
+    const handleFilter=(genre)=>{
+        setCurrGenre(genre);
+    }
     return (
         <div>
+        <div className="flex justify-center m-4">
+        {genrelist.map((genre)=>{
+            const isActive=currGenre===genre;
+            const baseStyles="flex justify-center items-center h-[3rem] w-[9rem] rounded-xl text-white font-bold mx-4";
+            const bgcolor=isActive ? "bg-blue-400":"bg-gray-400/50";
+            return <div className={`${baseStyles} ${bgcolor}`} onClick={()=>handleFilter(genre)}>{genre}</div>
+        })}
+        </div>
         <div className="flex justify-center my-10">
         <input 
             placeholder="Search Movies"
@@ -82,6 +102,13 @@ function WatchList(){
         </thead>
         <tbody className="divide-y divide-gray-100 border-t border-gray-100">
         {watchlist
+            .filter((movie)=>{
+                if(currGenre=="All Genres"){
+                    return true;
+                }else{
+                    return genreids[movie.genre_ids[0]]==currGenre;
+                }
+            })
             .filter((movie)=>{
                 return movie.title.toLowerCase().includes(search.toLowerCase());
             })
